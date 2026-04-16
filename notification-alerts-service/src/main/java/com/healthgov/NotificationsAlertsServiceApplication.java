f106bab917aabb23bd88756634b774ef6bdc1904
PRATAP0070 <aryanpratapsingh517@gmail.com>
2026-04-16 15:50:06 +0530

    initial commit

--- notification-alerts-service/src/main/java/com/healthgov/NotificationsAlertsServiceApplication.java
+++ notification-alerts-service/src/main/java/com/healthgov/NotificationsAlertsServiceApplication.java
@@ -8,6 +8,7 @@ public class NotificationsAlertsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationsAlertsServiceApplication.class, args);
+		
 	}
 
 }

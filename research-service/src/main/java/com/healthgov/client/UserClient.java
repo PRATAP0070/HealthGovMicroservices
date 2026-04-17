6e12136c1f8517b3ad61b3be5a8545564af68b31
Karan <karantalekar0777@gmail.com>
2026-04-17 10:46:55 +0530

    added dto and dependency

--- research-service/src/main/java/com/healthgov/client/UserClient.java
+++ research-service/src/main/java/com/healthgov/client/UserClient.java
@@ -4,9 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.healthgov.dto.UserReqDTO;
+
 @FeignClient(name = "auth-service")
 public interface UserClient {
 
-    @GetMapping("/users/{id}")
-    UserResponse getUserById(@PathVariable Long id);
+    @GetMapping("/healthGov/getUserById/{userId}")
+    UserReqDTO getUserById(@PathVariable("userId") Long userId);
 }
---
+++ research-service/src/main/java/com/healthgov/enums/Role.java
@@ -0,0 +1,6 @@
+package com.healthgov.enums;
+
+public enum Role {
+
+	CITIZEN,PROVIDER,RESEARCHER,MANAGER,ADMIN,COMPLIANCE,AUDITOR
+}

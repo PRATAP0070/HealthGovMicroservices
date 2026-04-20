102911e03f637b599525f54f94fa2c709d14646a
Prushottamverma <prushottamverma905@gmail.com>
2026-04-20 13:51:27 +0530

    Updated infrastructure, enrollment and resources

--- health-program-service/src/main/java/com/healthgov/dto/HealthProgramResponseDTO.java
+++ health-program-service/src/main/java/com/healthgov/dto/HealthProgramResponseDTO.java
@@ -2,8 +2,11 @@ package com.healthgov.dto;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 
+import com.healthgov.enums.InfrastructureType;
 import com.healthgov.enums.ProgramStatus;
+import com.healthgov.enums.ResourceType;
 
 import lombok.Data;
 
@@ -16,4 +19,34 @@ public class HealthProgramResponseDTO {
     private LocalDate endDate;
     private Double budget;
     private ProgramStatus status;
+    
+    private List<EnrollmentDTO> enrollments;
+	private List<ResourceDTO> resources;
+	private List<InfrastructureDTO> infrastructures;
+
+	@Data
+	public static class EnrollmentDTO {
+		private Long enrollmentId;
+		private Long citizenId;
+		private String citizenName;
+		private Date enrolledDate; // maps from Enrollment.date
+		private String status;
+	}
+
+	@Data
+	public static class ResourceDTO {
+		private Long resourceId;
+		private ResourceType type;
+		private Integer quantity;
+		private String status;
+	}
+
+	@Data
+	public static class InfrastructureDTO {
+		private Long infraId;
+		private InfrastructureType type;
+		private String location;
+		private Integer capacity;
+		private String status;
+	}
 }

7278c447683209d15093db8418d282998aaabe55
Prushottamverma <prushottamverma905@gmail.com>
2026-04-21 12:16:52 +0530

    Updated business logic

--- health-program-service/src/main/java/com/healthgov/dto/HealthProgramResponseDTO.java
+++ health-program-service/src/main/java/com/healthgov/dto/HealthProgramResponseDTO.java
@@ -4,23 +4,25 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.healthgov.enums.InfrastructureStatus;
 import com.healthgov.enums.InfrastructureType;
 import com.healthgov.enums.ProgramStatus;
+import com.healthgov.enums.ResourceStatus;
 import com.healthgov.enums.ResourceType;
 
 import lombok.Data;
 
 @Data
 public class HealthProgramResponseDTO {
-    private Long programId;
-    private String title;
-    private String description;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private Double budget;
-    private ProgramStatus status;
-    
-    private List<EnrollmentDTO> enrollments;
+	private Long programId;
+	private String title;
+	private String description;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private Double budget;
+	private ProgramStatus status;
+
+	private List<EnrollmentDTO> enrollments;
 	private List<ResourceDTO> resources;
 	private List<InfrastructureDTO> infrastructures;
 
@@ -28,25 +30,26 @@ public class HealthProgramResponseDTO {
 	public static class EnrollmentDTO {
 		private Long enrollmentId;
 		private Long citizenId;
-		private String citizenName;
-		private Date enrolledDate; // maps from Enrollment.date
+		LocalDate enrolledDate; // maps from Enrollment.date
 		private String status;
 	}
 
 	@Data
 	public static class ResourceDTO {
 		private Long resourceId;
+		private Long programId;
 		private ResourceType type;
 		private Integer quantity;
-		private String status;
+		private ResourceStatus status;
 	}
 
 	@Data
 	public static class InfrastructureDTO {
 		private Long infraId;
+		private Long programId;
 		private InfrastructureType type;
 		private String location;
-		private Integer capacity;
-		private String status;
+		private int capacity;
+		private InfrastructureStatus status;
 	}
 }

d4c2da338b11232a5fb1397039a26235cc9a929e
J-Sivakumar <jsivakumar321@gmail.com>
2026-04-28 10:52:55 +0530

    Added fallback method

---
+++ infrastructure-service/src/main/java/com/healthgov/exceptions/ProgramServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.healthgov.exceptions;
+
+public class ProgramServiceUnavailableException extends RuntimeException {
+	public ProgramServiceUnavailableException(String msg) {
+		super(msg);
+	}
+}
